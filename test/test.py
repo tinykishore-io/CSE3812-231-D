b66e5daab5fed5ef4a73d548a0ce9e92c8eb1df4
Kishorè Shanto <tinykishore@gmail.com>
2023-02-04 12:17:59 +0600

    fixed `test.py` for PyCharm

--- test/test.py
+++ test/test.py
@@ -4,15 +4,18 @@
 # What version of Python do you have?
 import sys
 
-# Ignore if tensorflow.keras is not installed in case of using JetBrains Software
+# Ignore if tensorflow.keras shows error in JetBrains Software (PyCharm)
 # Possible bug in PyCharm 2020.3.3
-import tensorflow.keras
+# import tensorflow.keras
+
+# Importing Keras fixed for PyCharm. Use: from tensorflow import keras
+from tensorflow import keras
 import pandas as pd
 import sklearn as sk
 import tensorflow as tf
 
 print(f"Tensor Flow Version: {tf.__version__}")
-print(f"Keras Version: {tensorflow.keras.__version__}")
+print(f"Keras Version: {keras.__version__}")
 print()
 print(f"Python {sys.version}")
 print(f"Pandas {pd.__version__}")
